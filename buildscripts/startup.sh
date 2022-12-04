d36811fe4c26fb5960fa7cfee5815c19e59520da
Jimurrito <jimurrito@gmail.com>
2022-12-04 00:37:27 -0800

    Working Build v1.12.3 - Needs finishing touches

--- buildscripts/startup.sh
+++ buildscripts/startup.sh
@@ -10,6 +10,10 @@ echo 'alias wksp="sh /wksp/root.sh"' >> ~/.bashrc
 # copy prv SSH key to user-accessible dir
 cp -v "$HOME"/.ssh/sshkey "$HOME"/keys/sshkey
 
+# Soft link for volumes
+ln -s /root/keys /keys
+ln -s /root/workspaces /workspaces
+
 # start vs code
 echo "Starting VSCode Server"
 code-server --accept-server-license-terms serve
