c8508845de0c77ad918ba43775562c5535156cb0
jimurrito <jimurrito@gmail.com>
2022-12-07 04:28:24 +0000

    fixed issue with git pre-config

--- buildscripts/user.sh
+++ buildscripts/user.sh
@@ -18,11 +18,11 @@ echo 'sh /buildscripts/greeting.sh' >> ~/.bashrc
 # Set git config
 # user.name
 if [ -n "$GIT_USER_NAME" ]; then
-    git config --global user.name "$GIT_USER_NAME"
+    git config --global user.name "$GIT_USER_NAME" >> ~/.bashrc
     echo "Git user name set: [$GIT_USER_NAME]"
 fi
 # user.email
 if [ -n "$GIT_USER_EMAIL" ]; then
-    git config --global user.email "$GIT_USER_EMAIL"
-    echo "Git user name set: [$GIT_USER_EMAIL]"
+    git config --global user.email "$GIT_USER_EMAIL" >> ~/.bashrc
+    echo "Git user name set: [$GIT_USER_EMAIL]" 
 fi
